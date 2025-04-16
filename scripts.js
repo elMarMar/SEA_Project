3adb0348d33a097b7256c8588442d0ca02c6bcce
Marvin <106219662+elMarMar@users.noreply.github.com>
2025-04-15 18:09:48 -0700

    fully working prototype

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 class Pokemon {
+  static lastId = 0;
   constructor(
     name,
     id,
@@ -30,118 +31,48 @@ class Pokemon {
   }
 }
 
-// "Grab" HTML elements and make them interative
-// TODO: FIX HEIGHT WEIGHT SEARCH
-const sortByNameBtn = document.getElementById("sort-by-name-btn");
-const sortByIdBtn = document.getElementById("sort-by-id-btn");
-
-// TODO: FIX HEIGHT WEIGHT SEARCH
-const sortByHeightBtn = document.getElementById("sort-by-height-btn");
-const sortByWeightBtn = document.getElementById("sort-by-weight-btn");
-
-const sortByHpBtn = document.getElementById("sort-by-hp-btn");
-const sortByAttackBtn = document.getElementById("sort-by-attack-btn");
-const sortByDefenseBtn = document.getElementById("sort-by-defense-btn");
-const sortBySpeedBtn = document.getElementById("sort-by-speed-btn");
-const searchInput = document.getElementById("site-search-box");
-const typeCheckboxes = document.getElementsByClassName("type-filter");
-const deselectAllTypesBtn = document.getElementById("deslect-all-types");
-const searchFilterBtn = document.getElementById("search-filter-btn");
-const reverseListBtn = document.getElementById("reverse-list-btn");
-const deleteBarInput = document.getElementById("delete-bar-input");
-const deleteBtn = document.getElementById("delete-btn");
-
-const toggleAdvancedSearchBtn = document.getElementById(
-  "toggle-advanced-search-btn"
-);
-
-let pokemonArr = []; // Work on pokemonArr rather than display cards themselves.
 const filters = {
-  sortedByAttribute: "",
   searchInput: "",
   types: [],
 };
 
-main();
+// Sort By UI
+const sortByNameBtn = document.getElementById("sort-by-name-btn");
+const sortByIdBtn = document.getElementById("sort-by-id-btn");
+// TODO: FIX HEIGHT WEIGHT SORT
+const sortByHeightBtn = document.getElementById("sort-by-height-btn");
+const sortByWeightBtn = document.getElementById("sort-by-weight-btn");
+const sortByHpBtn = document.getElementById("sort-by-hp-btn");
+const sortByAttackBtn = document.getElementById("sort-by-attack-btn");
+const sortByDefenseBtn = document.getElementById("sort-by-defense-btn");
+const sortBySpeedBtn = document.getElementById("sort-by-speed-btn");
+const reverseListBtn = document.getElementById("reverse-list-btn");
+// Filter By UI
+const typeCheckboxes = document.getElementsByClassName("type-filter");
+const deselectAllTypesBtn = document.getElementById("deslect-all-types");
+// Compact Menu UI
+const compactMenu = document.getElementsByClassName("main-menu")[0];
+const expandMenuButton = document.getElementById("toggle-main-menu-btn");
+const searchInput = document.getElementById("site-search-box");
+const searchFilterBtn = document.getElementById("search-filter-btn");
 
+const deleteBarInput = document.getElementById("delete-bar-input");
+const deleteBtn = document.getElementById("delete-btn");
+
+const addUpdateInputs = document.querySelectorAll(".add-update-input");
+const addBtn = document.getElementById("add-update-btn");
+// main() handles startup
 async function main() {
   data = await getData();
   storePokemonData(data);
-  showCards(data);
-
-  searchFilterBtn.addEventListener("click", applyFilters);
-  reverseListBtn.addEventListener("click", handleReverseList);
-  sortByNameBtn.addEventListener("click", function () {
-    handleSort("name", sortByNameBtn);
-  });
-  sortByIdBtn.addEventListener("click", function () {
-    handleSort("id");
-  });
-  sortByHeightBtn.addEventListener("click", function () {
-    handleSort("height");
-  });
-
-  sortByHpBtn.addEventListener("click", function () {
-    handleSort("hp");
-  });
-
-  sortByAttackBtn.addEventListener("click", function () {
-    handleSort("attack");
-  });
-
-  sortByDefenseBtn.addEventListener("click", function () {
-    handleSort("defense");
-  });
-
-  sortBySpeedBtn.addEventListener("click", function () {
-    handleSort("speed");
-  });
-
-  deselectAllTypesBtn.addEventListener("click", handleDeselectAllTypesBtn);
-  deleteBtn.addEventListener("click", handleDeletion);
-  toggleAdvancedSearchBtn.addEventListener("click", handleToggleAdvancedSearch);
+  showCards(pokemonArr);
+  setEventHandlers();
 }
 
 async function getData() {
   const res = await fetch("./data/pokedex.json");
   const data = await res.json();
   return data;
-}
-
-function applyFilters() {
-  storeFilters();
-  const targetValue = filters.searchInput;
-  const cards = document.getElementsByClassName("card");
-
-  for (let i = 0; i < pokemonArr.length; i++) {
-    const pm = pokemonArr[i];
-
-    // Filter By Search Bar
-    const searchBarMatch =
-      pm.name.trim().toLowerCase().includes(targetValue) ||
-      pm.species.trim().toLowerCase().includes(targetValue);
-
-    // Filter by types
-    const type1Match = filters.types.includes(pm.type1.toLowerCase());
-    let type2Match = false;
-    if (pm.type2 != null && filters.types.includes(pm.type2.toLowerCase()))
-      type2Match = true;
-
-    const typeMatch = type1Match || type2Match;
-
-    if (searchBarMatch && typeMatch) cards[i].classList.remove("hidden");
-    else cards[i].classList.add("hidden");
-  }
-}
-
-function storeFilters() {
-  filters.searchInput = searchInput.value.trim().toLowerCase(); // "Grab" string from searchbar
-  filters.types = []; // Reset type filter
-  // Instantiate Type Filter
-  for (let i = 0; i < typeCheckboxes.length; i++) {
-    if (typeCheckboxes[i].checked)
-      filters.types.push(typeCheckboxes[i].value.toLowerCase());
-  }
 }
 
 function storePokemonData(data) {
@@ -162,15 +93,14 @@ function storePokemonData(data) {
       temp.base.Defense,
       temp.base.Speed
     );
+    Pokemon.lastId++;
   }
 }
-
 function showCards(pokemon) {
   const cardContainer = document.getElementById("card-container");
   const templateCard = document.getElementById("template-card");
 
-  // Clear all the cards on screen except the template
-  cardContainer.innerHTML = "";
+  cardContainer.innerHTML = ""; // Clear all the cards on screen except the template
   cardContainer.appendChild(templateCard); // Keep the template in DOM but hidden
 
   for (let i = 0; i < pokemon.length; i++) {
@@ -195,7 +125,7 @@ function editCardContent(card, pokemon) {
 
   // Set Pokemon Name
   const cardName = card.getElementsByClassName("pokemon-name")[0];
-  cardName.textContent = pokemon.name;
+  cardName.textContent = pokemonName;
 
   // Set and Format Pokemon ID
   const cardId = card.getElementsByClassName("pokemon-id")[0];
@@ -231,25 +161,300 @@ function editCardContent(card, pokemon) {
   card.getElementsByClassName("pokemon-weight")[0].textContent =
     "Weight: " + pokemon.weight;
 
-  // Unfortunately Newer Pokemon Do NOT have their base stats available
-  // Check to see if they're available, if NOT --> display N/A
-  card.getElementsByClassName("pokemon-attack")[0].textContent =
-    "Attack: " + pokemon.attack;
+  // Set Base Stats
   card.getElementsByClassName("pokemon-hp")[0].textContent =
     "HP: " + pokemon.hp;
+  card.getElementsByClassName("pokemon-attack")[0].textContent =
+    "Attack: " + pokemon.attack;
   card.getElementsByClassName("pokemon-defense")[0].textContent =
     "Defense: " + pokemon.defense;
   card.getElementsByClassName("pokemon-speed")[0].textContent =
     "Speed: " + pokemon.speed;
 }
 
-function handleSort(attribute, toggleBtn) {
-  pokemonArr = quickSort(pokemonArr, attribute);
-  toggleBtn.classList.toggle("active");
-  console.log(toggleBtn);
+function setEventHandlers() {
+  // Event Handlers for Sort By UI
+  sortByNameBtn.addEventListener("click", function () {
+    handleSort("name", sortByNameBtn);
+  });
+  sortByIdBtn.addEventListener("click", function () {
+    handleSort("id", sortByIdBtn);
+  });
+  sortByHeightBtn.addEventListener("click", function () {
+    handleSort("height", sortByHeightBtn);
+  });
+
+  sortByWeightBtn.addEventListener("click", function () {
+    handleSort("weight", sortByWeightBtn);
+  });
+
+  sortByHpBtn.addEventListener("click", function () {
+    handleSort("hp", sortByHpBtn);
+  });
+
+  sortByAttackBtn.addEventListener("click", function () {
+    handleSort("attack", sortByAttackBtn);
+  });
+
+  sortByDefenseBtn.addEventListener("click", function () {
+    handleSort("defense", sortByDefenseBtn);
+  });
+
+  sortBySpeedBtn.addEventListener("click", function () {
+    handleSort("speed", sortBySpeedBtn);
+  });
+
+  reverseListBtn.addEventListener("click", handleReverseList);
+
+  // Event Handlers for Filter-By UI
+  deselectAllTypesBtn.addEventListener("click", handleDeselectAllTypesBtn);
+
+  // Event Handlers for Compact Menu UI
+  expandMenuButton.addEventListener("click", function () {
+    compactMenu.classList.toggle("hidden");
+  });
+  searchFilterBtn.addEventListener("click", applyFilters);
+  deleteBtn.addEventListener("click", handleDeletion);
+  addBtn.addEventListener("click", handleAddOrUpdatePokemon);
+}
+
+function handleAddOrUpdatePokemon() {
+  if (addUpdateInputs[11].value == "") addPokemon();
+  else updatePokemon(addUpdateInputs[11].value);
+  showCards(pokemonArr);
+}
+
+function addPokemon() {
+  // Check to make sure types are valid
+  let type1Val = addUpdateInputs[2].value.toLowerCase();
+  type1Val = capitalizeFirstLetter(type1Val);
+
+  let type2Val;
+  if (addUpdateInputs[3].value == "") type2Val = null;
+  else {
+    type2Val = addUpdateInputs[3].value.toLowerCase();
+    type2Val = capitalizeFirstLetter(type2Val);
+  }
+
+  let validType1 = false;
+  let validType2 = false;
+  for (let i = 0; i < typeCheckboxes.length; i++) {
+    if (typeCheckboxes[i].value == type1Val) validType1 = true;
+
+    if (typeCheckboxes[i].value == type1Val || type2Val == null)
+      validType2 = true;
+  }
+
+  let validTypes = validType1 && validType2;
+  if (!validTypes) {
+    alert("Type Values are invalid. Please try again");
+    return false;
+  }
+
+  let temp = new Pokemon(
+    addUpdateInputs[0].value,
+    ++Pokemon.lastId,
+    addUpdateInputs[1].value,
+    type1Val,
+    type2Val,
+    "./assets/images/black-square.png",
+    addUpdateInputs[4].value,
+    addUpdateInputs[5].value,
+    addUpdateInputs[6].value,
+    addUpdateInputs[7].value,
+    addUpdateInputs[8].value,
+    addUpdateInputs[9].value,
+    addUpdateInputs[10].value
+  );
+  pokemonArr.push(temp);
+  alert("New Pokemon has been added to the END of the pokedex!");
+  return true;
+}
+
+//TODO: WRITE THIS FUNCTION IN
+function updatePokemon(targetId) {
+  // Check to make sure types are valid
+  let type1Val = addUpdateInputs[2].value;
+  let type2Val = addUpdateInputs[3].value;
+  type1Val = capitalizeFirstLetter(type1Val);
+  type2Val = capitalizeFirstLetter(type2Val);
+  if (type2Val == "N/A") type2Val = null;
+
+  let validType1 = false;
+  let validType2 = false;
+
+  // This can very easily be optimized.
+  for (let i = 0; i < typeCheckboxes.length; i++) {
+    if (typeCheckboxes[i].value == type1Val || type1Val == "")
+      validType1 = true;
+
+    if (
+      typeCheckboxes[i].value == type1Val ||
+      type2Val == "" ||
+      type2Val == null
+    )
+      validType2 = true;
+  }
+
+  let validTypes = validType1 && validType2;
+  if (!validTypes) {
+    alert("Type Values are invalid. Please try again");
+    return false;
+  }
+
+  // Find Pokemon with Target ID
+  let i = 0;
+  let found = false;
+  for (; i < pokemonArr.length; i++) {
+    if (pokemonArr[i].id == targetId) {
+      found = true;
+      break;
+    }
+  }
+  if (!found) alert("No Pokemon with ID: " + targetId + " found.");
+  else {
+    updatePokemonAttribute(pokemonArr[i], "name", addUpdateInputs[0].value);
+    updatePokemonAttribute(pokemonArr[i], "species", addUpdateInputs[1].value);
+    updatePokemonAttribute(pokemonArr[i], "type1", type1Val);
+    updatePokemonAttribute(pokemonArr[i], "type2", type2Val);
+    updatePokemonAttribute(
+      pokemonArr[i],
+      "description",
+      addUpdateInputs[4].value
+    );
+    updatePokemonAttribute(pokemonArr[i], "height", addUpdateInputs[5].value);
+    updatePokemonAttribute(pokemonArr[i], "weight", addUpdateInputs[6].value);
+    updatePokemonAttribute(pokemonArr[i], "hp", addUpdateInputs[7].value);
+    updatePokemonAttribute(pokemonArr[i], "attack", addUpdateInputs[8].value);
+    updatePokemonAttribute(pokemonArr[i], "defense", addUpdateInputs[9].value);
+    updatePokemonAttribute(pokemonArr[i], "speed", addUpdateInputs[10].value);
+    alert("Updated Pokemon with ID: " + targetId + " sucessfully!");
+    return true;
+  }
+}
+
+function updatePokemonAttribute(pokemon, attribute, newVal) {
+  // Don't update if new values are not input
+  if (newVal == "") return;
+  pokemon[attribute] = newVal;
+}
+function handleDeletion() {
+  const target = deleteBarInput.value;
+  console.log(target);
+  deleteElementFromArr(pokemonArr, "name", target);
   applyFilters();
 }
 
+function deleteElementFromArr(arr, targetAttributeType, targetAttribute) {
+  let i = 0;
+  let found = false;
+  for (; i < arr.length; i++) {
+    if (arr[i][targetAttributeType] == targetAttribute) {
+      found = true;
+      break;
+    }
+  }
+
+  if (found) {
+    const toDelete = [arr[i].name, arr[i].id];
+    for (; i < arr.length - 1; i++) {
+      arr[i] = arr[i + 1];
+    }
+    pokemonArr.pop();
+    alert(
+      "Pokemon: " + toDelete[0] + "\nID: " + toDelete[1] + "\nHas been Deleted!"
+    );
+  } else {
+    alert(
+      "Pokemon " +
+        targetAttribute +
+        " Cannot Be Found!\nPokemon Deletion is CASE-SENSITIVE to prevent misdeletions."
+    );
+  }
+}
+
+function handleSort(attribute, toggleBtn) {
+  if (attribute == "height" || attribute == "weight") {
+    pokemonArr = quickSortByNumericValuesInString(pokemonArr, attribute);
+  } else pokemonArr = quickSort(pokemonArr, attribute);
+
+  const sortByBtns = document.querySelectorAll(".sort-by button");
+  console.log(sortByBtns);
+
+  for (let i = 0; i < sortByBtns.length; i++) {
+    sortByBtns[i].classList.remove("active");
+  }
+  toggleBtn.classList.add("active");
+  applyFilters();
+}
+
+function handleDeselectAllTypesBtn() {
+  let noValuesSelected = true;
+  for (let i = 0; i < typeCheckboxes.length; i++) {
+    if (typeCheckboxes[i].checked == true) {
+      noValuesSelected = false;
+      break;
+    }
+  }
+
+  if (noValuesSelected) {
+    for (let i = 0; i < typeCheckboxes.length; i++) {
+      typeCheckboxes[i].checked = true;
+    }
+  } else {
+    for (let i = 0; i < typeCheckboxes.length; i++) {
+      typeCheckboxes[i].checked = false;
+    }
+  }
+}
+
+function handleReverseList() {
+  pokemonArr = reverseList(pokemonArr);
+  applyFilters();
+}
+
+function reverseList(arr) {
+  let temp = [];
+  for (let i = arr.length - 1; i >= 0; i--) {
+    temp.push(arr[i]);
+  }
+  return temp;
+}
+
+function applyFilters() {
+  storeFilters();
+  const targetValue = filters.searchInput;
+  let filtered = [];
+
+  for (let i = 0; i < pokemonArr.length; i++) {
+    p = pokemonArr[i];
+    if (
+      p.name.trim().toLowerCase().includes(targetValue) ||
+      p.species.trim().toLowerCase().includes(targetValue)
+    ) {
+      if (
+        filters.types.includes(p.type1.toLowerCase()) ||
+        (p.type2 != null && filters.types.includes(p.type2.toLowerCase()))
+      )
+        filtered.push(p);
+    }
+  }
+
+  showCards(filtered);
+}
+
+function storeFilters() {
+  filters.searchInput = searchInput.value.trim().toLowerCase(); // "Grab" string from searchbar
+  filters.types = []; // Reset type filter
+
+  // Instantiate Type Filter
+  for (let i = 0; i < typeCheckboxes.length; i++) {
+    if (typeCheckboxes[i].checked)
+      filters.types.push(typeCheckboxes[i].value.toLowerCase());
+  }
+}
+
+// Data Structure Manipulation
 function quickSort(arr, attribute) {
   if (arr.length <= 1) return arr;
 
@@ -274,71 +479,43 @@ function quickSort(arr, attribute) {
   ];
 }
 
-function handleReverseList() {
-  pokemonArr = reverseList(pokemonArr);
-  applyFilters();
-}
+function quickSortByNumericValuesInString(arr, attribute) {
+  if (arr.length <= 1) return arr;
 
-function reverseList(arr) {
-  let temp = [];
-  for (let i = arr.length - 1; i >= 0; i--) {
-    temp.push(arr[i]);
-  }
-  return temp;
-}
+  const pivotPokemon = arr[arr.length - 1];
+  const pivot = getNumberOutOfString(pivotPokemon[attribute]);
 
-function handleToggleAdvancedSearch() {
-  const compactMenu = document.getElementsByClassName("main-menu")[0];
-  compactMenu.classList.toggle("hidden");
-}
+  const leftArr = [];
+  const rightArr = [];
 
-function handleDeletion() {
-  //TODO: HANDLE DELETION WITH BUTTON
-  const target = deleteBarInput.value;
-  console.log(target);
-  deleteElementFromArr(pokemonArr, "name", target);
-  showCards(pokemonArr);
-  console.log(pokemonArr.length);
-}
-
-function handlePokemonAddition() {}
-
-function deleteElementFromArr(arr, targetAttributeType, targetAttribute) {
-  let i = 0;
-  let found = false;
-  for (; i < arr.length; i++) {
-    if (arr[i][targetAttributeType] == targetAttribute) {
-      found = true;
-      break;
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (getNumberOutOfString(arr[i][attribute]) < pivot) {
+      leftArr.push(arr[i]);
+    } else {
+      rightArr.push(arr[i]);
     }
   }
 
-  if (found) {
-    for (; i < arr.length - 1; i++) {
-      arr[i] = arr[i + 1];
-    }
-    pokemonArr.pop();
-  } else return false;
-  return true;
+  return [
+    ...quickSortByNumericValuesInString(leftArr, attribute),
+    pivotPokemon,
+    ...quickSortByNumericValuesInString(rightArr, attribute),
+  ];
 }
 
-function handleDeselectAllTypesBtn() {
-  console.log(typeCheckboxes.length);
-  let noValuesSelected = true;
-  for (let i = 0; i < typeCheckboxes.length; i++) {
-    if (typeCheckboxes[i].checked == true) {
-      noValuesSelected = false;
-      break;
-    }
+// Formatting Functions:
+function capitalizeFirstLetter(string) {
+  if (typeof string !== "string" || string.length === 0) {
+    return ""; // Return empty string for non-string input or empty string
   }
-
-  if (noValuesSelected) {
-    for (let i = 0; i < typeCheckboxes.length; i++) {
-      typeCheckboxes[i].checked = true;
-    }
-  } else {
-    for (let i = 0; i < typeCheckboxes.length; i++) {
-      typeCheckboxes[i].checked = false;
-    }
-  }
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+function getNumberOutOfString(string) {
+  let result = string.replace(/[^0-9.]/g, "");
+  return parseFloat(result);
+}
+
+// Running Code:
+let pokemonArr = [];
+main();
